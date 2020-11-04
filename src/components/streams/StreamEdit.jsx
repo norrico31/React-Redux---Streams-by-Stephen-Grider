91bc6cf2944c6e6680c582fd8cf0a82f723519fc
norrico31 <geraldbiasonjob31.com>
2020-11-04 23:07:07 +0800

    code reuse in StreamForm for StreamEdit and StreamCreate

--- src/components/streams/StreamEdit.jsx
+++ src/components/streams/StreamEdit.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from 'react'
+import { pick } from 'lodash'
+import React from 'react'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id)
     }
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues)
+    }
     render() {
-        console.log(this.props)
         if (!this.props.stream) return <div>Loading...</div>
         return (
             <div>
-                {this.props.stream.title}
+                <h3>Edit a Stream</h3>
+                <StreamForm initialValues={pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
             </div>
         )
     }
 }
 
-const mapStateToProps = (state, ownProps) => {
-    return { stream: state.streams[ownProps.match.params.id] }
+const mapStateToProps = ({ streams }, ownProps) => {
+    return { stream: streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
